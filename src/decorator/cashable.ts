818f79363bcacd453fba3dec099ae18df82051ab
12tare21 <12tare21@gmail.com>
2021-04-27 23:49:58 +0200

    Implement methods injection through property decorator

---
+++ src/decorator/cashable.ts
@@ -0,0 +1,42 @@
+import { CashableFactory } from '../factory/cashable.factory';
+import Stripe from 'stripe';
+import { CashableMethodCollector } from '../collector/cashable-method.collector';
+
+export function Cashable(stripe: Stripe, options: {
+    currency: string,
+    customerIdAlias?: string,
+    email?: string,
+}): PropertyDecorator {
+    const customerIdAlias: string = options.customerIdAlias ?? 'stripeId';
+    const emailAlias: string = options.email ?? 'email';
+    const currency: string = options.currency;
+
+    return function (object, propertyKey: string) {
+        const factory: CashableFactory = new CashableFactory();
+        Object.defineProperty(object, propertyKey, {
+            configurable: false,
+            get: function (this: { [name: string]: any }) {
+                const email: string = this[emailAlias];
+                const customerId: string = this[customerIdAlias];
+
+                if (!this.hasOwnProperty(customerIdAlias)) {
+                    throw new Error('Cashable property containing customer ID must be declared and defined.')
+                }
+                return new CashableMethodCollector().buildMethods.call(this, stripe, customerIdAlias, customerId, currency, email);
+                // return factory.generate.call(this, stripe, customerIdAlias, customerId, currency, email)
+            },
+            set: function (this: { [name: string]: any }) {
+                const email: string = this[emailAlias];
+                const customerId: string = this[customerIdAlias];
+                
+                if (!this.hasOwnProperty(customerIdAlias)) {
+                    throw new Error('Property containing customer ID must be declared and defined.')
+                }
+                return new CashableMethodCollector().buildMethods.call(this, stripe, customerIdAlias, customerId, currency, email);
+                // return factory.generate.call(this, stripe, customerIdAlias, customerId, currency, email)
+            }
+        });
+
+        return object;
+    }
+}
